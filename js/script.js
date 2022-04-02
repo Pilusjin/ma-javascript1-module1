240c2a0acc39f308482f1539bf2a285685778c55
Pilusjin <75672659+Ilusjin@users.noreply.github.com>
2022-04-02 13:06:42 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,66 @@ const cats = [
     }
 ];
 
+// Question 1
+
+const cat = {
+    complain: function(){
+        console.log("meow!");
+    }
+}
+
+cat.complain();
+
+// Question 2
+
+const heading = document.querySelector("h3")
+heading.innerHTML = "Updated heading";
+
+// Question 3
+
+heading.style.fontSize = "2em";
+
+// Question 4
+document.className =" subheading";
+
+// Question 5
+
+const paragraphs = document.querySelectorAll("p")
+for(let i=0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "red";
+}
+// Question 6
+
+const resultsContainer = document.querySelector(".results")
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.background = "yellow"
+
+// Question 7
+
+function dildo(list) {
+    for(let i =0; i < list.length; i++) {
+        console.log(list[i].name);
+    }
+}
+dildo(cats)
+
+// Question 8
+
+function creatsCats (cats) {
+    let html = "";
+    for ( let i = 0; i < creatsCats.length; i++){
+        let catAge = "Age";
+        if (creatsCats[i].age) {
+            catAge = creatsCats[i].age;
+        }
+        html += `<div class="cats">
+                <h5${creatsCats[i].name}</h5>
+                <p>${creatsCats[i].age}</p>
+                </div>`;
+    }
+
+    return html;
+}
+
+const newHTML = creatsCats(cats);
+const 
